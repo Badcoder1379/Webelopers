0d7c03a0f883efab58afdc7029df122c90d27a75
M.H Gheisarieh <mgheysariyeh@gmail.com>
2019-11-07 17:02:21 +0330

    6

--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 from home.forms import SignUpForm
@@ -11,11 +13,25 @@ def homePage(request):
 
 
 def register(request):
+    Error = ''
     if request.method == "POST":
         form = SignUpForm(request.POST)
+        if form.password1 != form.password2:
+            Error = 'NotEqualPass'
+        users = User.objects.filter(username=form.username)
+        if len(users) != 0:
+            Error = 'ExistedUser'
         form.save()
-    return render(request, 'register.html')
+
+    return render(request, 'register.html', {'error': Error})
 
 
 def sign_in(request):
-    return render(request, 'sign_in.html')
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return render(request, 'logged_in.html')
+    else:
+        return render(request, 'sign_in.html', {'invalid': False})
