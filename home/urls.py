80cb5feb9de5107c6afda56441fec0a2b0cbb390
M.H Gheisarieh <mgheysariyeh@gmail.com>
2019-11-07 16:13:50 +0330

    register

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from django.urls import path
+from django.contrib.auth.models import *
+
 
 from home import views
 
 urlpatterns = [
     url('register/', views.register, name="register"),
     url('sign_in/', views.sign_in, name='sign_in'),
-    url('', views.homePage, name="home"),
+    url('', views.homePage, name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
 
+
+
 def homePage(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        form.save()
     return render(request, 'home.html')
+
 
 def register(request):
     return render(request, 'register.html')
