9e2652466fdaf7e453a2acb66a9c85516353112e
Mohammad Heidary <heidary_1379@yahoo.com>
2019-11-07 18:32:41 +0330

    11

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from django.contrib.auth.models import *
 
-
 from home import views
 
 urlpatterns = [
     url('register/', views.register, name="register"),
     url('sign_in/', views.sign_in, name='sign_in'),
-    url('', views.homePage, name='home'),
     url('login/', views.login, name='login'),
     url('contact_us_done/', views.contact_us_done, name='contact_us_done'),
+    url('', views.homePage, name='home'),
+
 ]
