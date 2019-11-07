4e5caf57a2adaa93de67ca42ce4bf046d8e35d7b
M.H Gheisarieh <mgheysariyeh@gmail.com>
2019-11-07 16:24:57 +0330

    6

--- home/views.py
+++ home/views.py
@@ -1,23 +1,21 @@
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 
+from home.forms import UserCreationForm
 
 # Create your views here.
 
 
-
-
 def homePage(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        form.save()
     return render(request, 'home.html')
 
 
 def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        form.save()
     return render(request, 'register.html')
+
+
 
 def sign_in(request):
     return render(request, 'sign_in.html')
